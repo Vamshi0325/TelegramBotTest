d56db53be3894d692e82bce901096496d13252a9
Vamshi <vamshivade2000@gmail.com>
2025-02-10 16:28:22 +0530

    Update NotMobileDevice.jsx
    text changed

--- src/NotMobileDevice.jsx
+++ src/NotMobileDevice.jsx
@@ -5,7 +5,7 @@ const NotMobileDevice = () => {
     return (
         <div style={{ background: "linear-gradient(to bottom, #000428, #004e92", width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <img src={Image} alt="" style={{ width: '100%', height: '200px', objectFit: 'contain' }} />
-            <h2 style={{ color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px 20px', textAlign: 'center', }}>To Play Games Please use a Mobile device</h2>
+            <h2 style={{ color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px 20px', textAlign: 'center', }}>Testing</h2>
         </div>
     );
 };
