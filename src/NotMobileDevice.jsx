2235e54259248b5f0f7daba8adbd67af7df94494
VAMSHI VADE. <vamshiv@STRING.METAVERSE>
2025-02-10 17:28:07 +0530

    test commit

--- src/NotMobileDevice.jsx
+++ src/NotMobileDevice.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Image from './notFound.png'
+import './App.css';
 
 const NotMobileDevice = () => {
     return (
         <div style={{ background: "linear-gradient(to bottom, #000428, #004e92", width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <img src={Image} alt="" style={{ width: '100%', height: '200px', objectFit: 'contain' }} />
-            <h2 style={{ color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '10px 20px', textAlign: 'center', }}>To Play Games Please use a Mobile device</h2>
+            <h2>To Play Games Please use a Mobile device</h2>
         </div>
     );
 };
