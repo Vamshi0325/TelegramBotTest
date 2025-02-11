e37d81696aa5c4ef9f2851b262b687cff62495bb
VAMSHI VADE. <vamshiv@STRING.METAVERSE>
2025-02-11 15:07:56 +0530

    cd

--- src/NotMobileDevice.jsx
+++ src/NotMobileDevice.jsx
@@ -4,7 +4,7 @@ import './App.css';
 
 const NotMobileDevice = () => {
     return (
-        <div style={{ border: '10px solid red', background: 'linear-gradient(to bottom, #000428, #004e92)', width: '100%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+        <div className='not-mobile'>
             <img src={Image} alt="" style={{ width: '100%', height: '200px', objectFit: 'contain' }} />
             <h2>To Play Games Please use a Mobile device</h2>
         </div>
